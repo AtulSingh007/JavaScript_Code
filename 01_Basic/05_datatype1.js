b8d55039a9d545ff98879eaaae2f502c98524f2a
Atul Singh <49321624+AtulSingh007@users.noreply.github.com>
2024-02-26 20:22:03 +0000

    dataype_summary

---
+++ 01_Basic/05_datatype1.js
@@ -0,0 +1,43 @@
+//Arithmetic Operator
+//1. Assignment opertaor:-
+let x=10;
+let y=2;
+let z=x+y;
+
+//Addition
+
+//1. string 
+//2. number
+//3.bigint
+//4.boolean
+//5.undefined
+//6.null
+//7.symbol
+//8.object
+
+//object:-
+//1.array
+//2.object
+//3.datetime
+
+// 1.Number
+let max=30;
+let length=40;
+
+//object
+const person ={name:"Atul",age:"25"};
+
+//array
+const car =["BMW","Maruti","hoonda"];
+
+// date
+const date =new date("2024-02-27");
+
+//function
+const myfunction = function()
+{
+    console.log("Hello World!!");
+}
+// array
+const city =["delhi","mumbai","UP"];
+
